a026765c95d6d0eb72a0e1965f8c9c34e29655db
Emma Boys <eboys@uoregon.edu>
2016-04-01 09:12:10 -0700

    Delete scripts.js

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-var question = "What is Superman's real name?"
-var answer = prompt(question);
-var answer = answer.toLowerCase(answer);
-alert("You answered " + answer);
-if(answer == "clark kent"){
-	alert("That's the correct answer");
-} else {
-	alert("The correct anwer was Clark Kent");
-}
